528756530c473f0c6019d84d3c8670050b6ac724
Alex Gaudiosi <aa.gaudiosi@gmail.com>
2014-03-18 14:57:26 +0000

    flash message added

--- sudoku.rb
+++ sudoku.rb
@@ -1,5 +1,7 @@
 require 'sinatra'
 require 'sinatra/partial'
+require 'rack-flash'
+use Rack::Flash
 set :partial_template_engine, :erb
 
 require_relative './lib/sudoku'
@@ -68,6 +70,9 @@ end
 
 def prepare_to_check_solution
 	@check_solution = session[:check_solution]
+	if @check_solution
+			flash[:notice] = "Incorrect values are highlighted in yellow"
+		end
 	session[:check_solution] = nil
 end
 
